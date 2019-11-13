cb3719e9beef818b0801e451096c96024b4947cd
wdorau41 <54552856+wdorau41@users.noreply.github.com>
2019-11-13 06:23:53 -0600

    added the code from google to my script page and gps html page

--- scripts.js
+++ scripts.js
@@ -3,4 +3,12 @@ function play(vid){
   vid.autoplay = true;
   vid.load();
 }
-vid.addEventListener('load', play);
+vid.addEventListener(window, 'load', play);
+
+var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8787, lng: 87.7403},
+    zoom: 8
+    });
+  }
